8ed11c5f8ad0ab8894bdf2c19ac07cd739b856c0
Giorgi Mosemgvdlishvili <gmosemgvdlishvili28@gmail.com>
2022-11-23 13:08:00 +0400

    update axios

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const  API_URL = "http://localhost:5000/api"
+export const  API_URL = "https://aaaapppp12.onrender.com/api"
 const axiosClient = axios.create({
     withCredentials: true,
     baseURL: API_URL
